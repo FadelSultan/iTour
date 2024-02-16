a3ecdf9bd3340fab78d3aaf2f5906029590280a8
FADEL SULTAN <srqa3h.com@gmail.com>
2024-02-16 16:18:52 +0300

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Fadel Sultan on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
